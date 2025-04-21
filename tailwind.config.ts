e1d0f5ba904732a09ddea4fbe5ff9f56a2e76b6f
lanntxyz <lannt.xyz@gmail.com>
2025-04-21 20:30:00 +0700

    Add Tailwind CSS configuration and update TypeScript settings for improved styling and module handling

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+import type { Config } from "tailwindcss";
+
+const tailwindConfig: Config = withMT({
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+    },
+  },
+  plugins: [
+  ],
+}) as Config;
+
+export default tailwindConfig satisfies Config;
